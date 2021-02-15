56d5b340953f5327fefa7c5fe93ac336e8afa154
kayo pereira <kayo.richard2000@gmail.com>
2021-02-15 18:33:12 -0300

    challenge 03 in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,16 @@
+require "open-uri"
+require "json"
+
 class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
-    raise
+    @user_score = params[:value]
+    response = open("https://wagon-dictionary.herokuapp.com/#{@user_score}")
+    @json = JSON.parse(response.read)
+    @json['found']
+    @json['word']
   end
 end
